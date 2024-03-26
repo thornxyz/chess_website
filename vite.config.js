dae0761008398e4ff8b5e281a2d470d9e2ca7853
Thorn <subhrajyoti.ghosh@outlook.com>
2024-03-26 18:37:20 +0530

    vercel test

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/chess_website/"
+  base: process.env.VITE_BASE_PATH || "/chess_website/"
 })
